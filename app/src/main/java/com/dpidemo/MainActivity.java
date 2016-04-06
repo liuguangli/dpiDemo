12b16fec437fb190075a9c3bc885e2bd13d6efcd
liuguangli <244576106@qq.com>
2016-04-06 10:42:02 +0800

    init

---
+++ app/src/main/java/com/dpidemo/MainActivity.java
@@ -0,0 +1,74 @@
+package com.dpidemo;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        TextView currentDensity = (TextView) findViewById(R.id.density);
+        currentDensity.setText("当前屏幕密度:"+getScreenSize(this).density);
+    }
+    public static DisplayMetrics getScreenSize(Context context){
+        DisplayMetrics displayMetrics;
+
+            try {
+
+                displayMetrics = new DisplayMetrics();
+                displayMetrics.widthPixels = 1280;
+                displayMetrics.heightPixels = 720;
+                WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+                wm.getDefaultDisplay().getMetrics(displayMetrics);
+            } catch (Exception e){
+                return null;
+            }
+
+
+        return displayMetrics;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
